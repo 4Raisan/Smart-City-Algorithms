43a66f5bfa71e184474b54934c477963603acbaa
Rashen Anupama <mn4raisan@gmail.com>
2026-02-22 09:25:52 +0530

    comments on BubbleSort

--- src/Module_02/BubbleSort.java
+++ src/Module_02/BubbleSort.java
@@ -3,19 +3,20 @@ package Module_02;
 public class BubbleSort {
 
     public static long bubbleSort(int[] arr) {
-        long startTime = System.nanoTime();
-        boolean swap = false;
-        for (int i=0; i<arr.length-1; i++) {
-            for (int k=0; k<arr.length-1; k++) {
+        long startTime = System.nanoTime();   // get current time
+        boolean swap = false;  //  detect to finish sorting
+
+        for (int i=0; i<arr.length-1; i++) {    // outer loops
+            for (int k=0; k<arr.length-1; k++) {       // inner loops
                 if (arr[k] > arr[k+1]) {
-                        int temp = arr[k];
-                        arr[k] = arr[k+1];
+                        int temp = arr[k];   // hold current index
+                        arr[k] = arr[k+1];  // swap values on given array
                         arr[k+1] = temp;
                         swap = true;
                  }
             }
             if (!swap) {break;}
         }
-        return System.nanoTime() - startTime;   // currentTime -
+        return System.nanoTime() - startTime;   // calculate and return time
     }
 }
