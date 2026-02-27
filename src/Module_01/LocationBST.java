405f9dacc97d5f6d5c89b4e3209264eee7d185d9
Imesh <ikaushalya7172800@gmail.com>
2026-02-27 09:35:01 +0530

    clean code

--- src/Module_01/LocationBST.java
+++ src/Module_01/LocationBST.java
@@ -13,7 +13,35 @@ public class LocationBST {
         }
     }
 
+    private  TreeNode root;
 
+    public void insert (String name){
+        root = insertRec (root, name);
+    }
+
+    private TreeNode insertRec(TreeNode root, String name){
+
+        if (root == null)
+            return new TreeNode(name);
+
+        if (name.compareTo(root.name) < 0)
+            root.left = insertRec(root.left, name);
+        else if (name.compareTo(root.name) > 0)
+            root.right = insertRec(root.right, name);
+
+        return root;
+    }
+
+    public  void inorder(){
+        inorder(root);
+    }
+    private void inorder(TreeNode root){
+        if (root == null){
+            inorderRec(root.left);
+
+
+        }
+    }
 
 
 
