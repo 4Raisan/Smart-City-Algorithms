e1128edf6aba15d42e53a7f38c4d14b369268af3
Rashen Anupama <mn4raisan@gmail.com>
2026-02-28 02:47:35 +0530

    manu implement on sort manager

--- src/Module_02/SortManager.java
+++ src/Module_02/SortManager.java
@@ -2,5 +2,11 @@ package Module_02;
 import java.util.*;
 
 public class SortManager {
+    public static void start() {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 - Random Array");
+        System.out.println("2 - Manual Input");
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
 }
