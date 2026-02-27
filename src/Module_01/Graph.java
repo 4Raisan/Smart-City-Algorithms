37b9f8caf25770fd3c800c54f8aca37bb641cae1
Imesh <ikaushalya7172800@gmail.com>
2026-02-27 11:03:00 +0530

    clean code

--- src/Module_01/Graph.java
+++ src/Module_01/Graph.java
@@ -1,4 +1,26 @@
 package src.Module_01;
+import java.util.*;
 
 public class Graph {
+    private Map < String, List< String > > adjList;
+
+    public Graph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addLocation(String location) {
+        adjList.putIfAbsent(location, new ArrayList<>());
+    }
+
+    public void removeLocation(String location){
+        adjList.remove(location);
+        for (List<String> neighbors : adjList.values()){
+            neighbors.remove(location);
+        }
+    }
+
+    public void addEdge(String from, String to) {
+    }
+
+
 }
