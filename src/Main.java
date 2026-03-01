9a4f4a0b75873be16f42e423b9368521ebdabc67
Imesh <ikaushalya7172800@gmail.com>
2026-03-01 18:39:56 +0530

    implementing main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import Module_01.*;
 import Module_02.SortManager;
 
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class  Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
+        do {
+            //menu display (all 3)
+            System.out.println("\n ================================");
+            System.out.println("        Smart City Algorithms       ");
+            System.out.println("===============================");
+
+
+
+
+
+
+
+        }
+
+        //
+        // so
+        // System
+        //
+        //
+        //
+        //
         // Module 2
         // ...
 
