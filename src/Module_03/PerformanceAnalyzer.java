022595b49c4befcb15647c0faba8f02482154cd1
Hasitha boluakandura <lakshanh717@gamil.com>
2026-03-01 17:12:40 +0530

    Implementation of PerformanceAnalyzer

--- src/Module_03/PerformanceAnalyzer.java
+++ src/Module_03/PerformanceAnalyzer.java
@@ -19,36 +19,4 @@ public class PerformanceAnalyzer {
         return copy;
     }
 
-    public static void main(String[] args) {
-
-        int[] sizes = {100, 500, 1000};
-
-        System.out.println("==============================================================");
-        System.out.println("Size\tLinear Search (ns)\tBubble Sort (ns)");
-        System.out.println("==============================================================");
-
-        for (int size : sizes) {
-
-            int[] arr = generateArray(size);
-            int[] arrForSort = copyArray(arr);
-
-            int key = arr[size - 1]; // Worst case for linear search
-
-            // Measure Linear Search
-            long startSearch = System.nanoTime();
-            SearchingAlgorithms.linearSearch(arr, key);
-            long endSearch = System.nanoTime();
-            long searchTime = endSearch - startSearch;
-
-            // Measure Bubble Sort
-            long startSort = System.nanoTime();
-            SortingAlgorithms.bubbleSort(arrForSort);
-            long endSort = System.nanoTime();
-            long sortTime = endSort - startSort;
-
-            System.out.println(size + "\t" + searchTime + "\t\t\t" + sortTime);
-        }
-
-        System.out.println("==============================================================");
-    }
 }
---
+++ src/Module_03/SearchingAlgorithms.java
@@ -0,0 +1,13 @@
+package src.Module_03;
+
+public class SearchingAlgorithms {
+
+    public static int linearsearch (int [] arr, int key) {
+        for(int i=0; i< arr.length; i++ ) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
